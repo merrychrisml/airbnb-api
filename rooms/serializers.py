3c41406ce51214d790af3681d12a816b873cc1c2
merrychrisml <80391881+merrychrisml@users.noreply.github.com>
2021-09-22 15:47:52 +0900

    DRF finish

--- rooms/serializers.py
+++ rooms/serializers.py
@@ -1,12 +1,19 @@
 from rest_framework import serializers
-from .models import Room
+from .models import Room, Photo
 from users.serializers import UserSerializer
+
+
+class PhotoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Photo
+        exclude = ("room",)
 
 
 class RoomSerializer(serializers.ModelSerializer):
 
-    user = UserSerializer(read_only=True)
     is_fav = serializers.SerializerMethodField()
+    user = UserSerializer(read_only=True)
+    photos = PhotoSerializer(read_only=True, many=True)
 
     class Meta:
         model = Room
--- users/views.py
+++ users/views.py
@@ -23,7 +23,11 @@ class UsersViewSet(ModelViewSet):
         permission_classes = []
         if self.action == "list":
             permission_classes = [IsAdminUser]
-        elif self.action == "create" or self.action == "retrieve":
+        elif (
+            self.action == "create"
+            or self.action == "retrieve"
+            or self.action == "favs"
+        ):
             permission_classes = [AllowAny]
         else:
             permission_classes = [IsSelf]
@@ -45,19 +49,16 @@ class UsersViewSet(ModelViewSet):
         else:
             return Response(status=status.HTTP_401_UNAUTHORIZED)
 
-
-class FavsView(APIView):
-
-    permission_classes = [IsAuthenticated]
-
-    def get(self, request):
-        user = request.user
+    @action(detail=True)
+    def favs(self, request, pk):
+        user = self.get_object()
         serializer = RoomSerializer(user.favs.all(), many=True).data
         return Response(serializer)
-
-    def put(self, request):
+    
+    @favs.mapping.put
+    def toggle_favs(self, request, pk):
         pk = request.data.get("pk", None)
-        user = request.user
+        user = self.get_object()
         if pk is not None:
             try:
                 room = Room.objects.get(pk=pk)
